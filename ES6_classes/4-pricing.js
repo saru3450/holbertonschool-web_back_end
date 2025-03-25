6919076d1c9f52ea7e4f9668f47b0cb7f9bc25a2
saru3450 <9971@holbertonstudents.com>
2025-03-25 16:32:46 +0000

    4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,54 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter pour 'amount'
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter pour 'amount'
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  // Getter pour 'currency'
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter pour 'currency'
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  // Méthode pour afficher le prix comple
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Méthode statique pour convertir un pri
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
