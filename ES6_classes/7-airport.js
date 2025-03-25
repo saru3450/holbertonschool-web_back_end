9a39158dd2d4925cea325123524b19abed172ec9
saru3450 <9971@holbertonstudents.com>
2025-03-25 16:42:35 +0000

    7-airport.js

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,34 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
