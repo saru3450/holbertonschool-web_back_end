e318dd142c4b18b200581bedc66a55525de9b7b4
saru3450 <9971@holbertonstudents.com>
2025-03-25 16:36:58 +0000

    5-building.js

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square footage must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  // Getter pour sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Méthode abstraite
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
