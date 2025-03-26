ad2f7c2279a4f43ef4d6d92c8994d040d746e79c
saru3450 <9971@holbertonstudents.com>
2025-03-26 14:49:39 +0000

    espaceerror

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
 import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
-	return [
-	 new ClassRoom(19),
+  return [
+    new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
   ];
