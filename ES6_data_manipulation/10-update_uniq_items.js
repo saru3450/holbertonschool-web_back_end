2ad2b0d380127848ac9132581fbae86c58d1f58e
saru3450 <9971@holbertonstudents.com>
2025-03-31 21:34:20 +0000

    10-update_uniq_items.js

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(map) {
+    if (!(map instanceof Map)) {
+      throw new Error('Cannot process');
+    }
+    for (const [list, value] of map.entries()) {
+      if (value === 1) {
+        map.set(list, 100);
+      }
+    }
+  }
