d30f675a792813868f7d0302c273d0621b5436f2
saru3450 <9971@holbertonstudents.com>
2025-04-03 07:47:40 +0000

    8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,8 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') {
     return '';
   }
-  
-return [...set]
-  .filter((value) => value.startsWith(startString))
-  .map((value) => value.slice(startString.length))
-  .join('-');
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
