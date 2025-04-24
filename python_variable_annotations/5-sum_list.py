6dc2396fc1a37f70d09694192aee773d878ed750
saru3450 <9971@holbertonstudents.com>
2025-04-24 13:38:16 +0000

    5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+function sum_list take input_list
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """function sum element of the list"""
+    return sum(input_list) 
