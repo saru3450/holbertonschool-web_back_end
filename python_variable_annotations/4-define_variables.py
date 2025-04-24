2f2629d966677002889460aec1a4eec6e012d8ad
saru3450 <9971@holbertonstudents.com>
2025-04-24 13:19:55 +0000

    4-define_variables.py

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+a, integer: 1
+pi, float: 3,14
+i_understand_annotations: boolean
+school: string "holberton"
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
