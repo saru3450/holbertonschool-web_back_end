03fbb974a760d7e4077ea19beaf5256945c42862
saru3450 <9971@holbertonstudents.com>
2025-04-28 14:24:22 +0000

    0-async_generator.py

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Coroutine qui génère 10 nombs aléatoires entre 0 et 1
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
