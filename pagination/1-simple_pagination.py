2d14d37f096be91955b991d01eaf3af7accc68c9
saru3450 <9971@holbertonstudents.com>
2025-05-02 20:55:12 +0000

    1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-Copy index_range
+Function return the tuple with 2 argument:
+page and page_size
+and do a simple pagination
 """
 import csv
 import math
@@ -8,7 +10,7 @@ from typing import List
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """ return a tuple of size two containing a start index and an end index"""
+    """the simple helper function"""
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
@@ -34,6 +36,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """simple pagination"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
