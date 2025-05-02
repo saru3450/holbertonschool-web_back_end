ab6c62844ebcc327414b69b90bc7f0d26045a401
saru3450 <9971@holbertonstudents.com>
2025-05-02 21:02:56 +0000

    2-hypermedia_pagination.py

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -8,7 +8,7 @@ from typing import List, Dict, Any
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """ return a tuple of size two containing a start index and an end index"""
+    """return a tuple of size two containing a start index and an end index"""
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
@@ -34,6 +34,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """simple pagination"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
@@ -42,20 +43,17 @@ class Server:
 
         if first_index >= len(data):
             return []
-
         return data[first_index:last_index]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-        """returns a dictionary containing the following key-value pairs"""
-        data = self.get_page(page, page_size)
-        total_items = len(self.dataset())
-        total_pages = math.ceil(total_items / page_size)
-
+        """ returns a dictionary containing the following key-value pairs """
+        data_page = self.get_page(page, page_size)
+        totalPages = math.ceil(len(self.dataset()) / page_size)
         return {
-            'page_size': len(data),
+            'page_size': len(data_page),
             'page': page,
-            'data': data,
-            'next_page': page + 1 if page < total_pages else None,
+            'data': data_page,
+            'next_page': page + 1 if page < totalPages else None,
             'prev_page': page - 1 if page > 1 else None,
-            'total_pages': total_pages
+            'total_pages': totalPages
         }
