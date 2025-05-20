de5407157d5fb64722bdb4c761691db61fd2779d
saru3450 <9971@holbertonstudents.com>
2025-05-20 13:23:44 +0000

    1-stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+console.log("Welcome to Holberton School, what is your name?");
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+  console.log("This important software is now closing");
+  process.exit();
+});
