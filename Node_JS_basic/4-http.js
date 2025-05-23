526e749192b6e9d7dfd19d8e650649ddfb73bf6a
saru3450 <9971@holbertonstudents.com>
2025-05-23 20:00:30 +0000

    4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,13 +1,11 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200; // Statut HTTP : OK
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
